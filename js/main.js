75823e8ebfa2f31700b2a29f14e2ec9fafc27aee
Greg Ells <gregells@gmail.com>
2023-10-06 13:54:28 +1300

    buttons and uppercase

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-console.log('hey');
+/*----- constants -----*/
+
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+
